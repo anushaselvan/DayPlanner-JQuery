099fd6853e1b3ee082307586493b5f494d806489
Anusha <anushaselvan@gmail.com>
2021-09-24 00:19:42 +1000

    Final working code

--- script.js
+++ script.js
@@ -1,44 +1,12 @@
 var today = moment().format("LLLL");
 $("#currentDay").text(today);
-/*var txtArea10 = $("textarea");
-var txtArea11 = $("textarea");
-var txtArea12 = $("textarea");
-var txtArea13 = $("textarea");
-var txtArea14 = $("textarea");
-var txtArea15 = $("textarea");
-var txtArea17 = $("textarea");
-var txtArea18 = $("textarea");
-var txtArea = [ "txtArea9","txtArea10","txtArea11","txtArea12","txtArea13", "txtArea14", "txtArea15", "txtArea16", "txtArea17"];
-console.log(txtArea);
-var row9 = $("#9");
-var row10 = $("#10");
-var row11 = $("#11");
-row11 = 11;
-var row12 = $("#12");
-row12 = 12;
-var row13 = $("#13"); 
-row13 = 13;
-var row14 = $("#14");
-row14 = 14;
-var row15 = $("#15");
-row15 = 15;
-var row16 = $("#16");
-row16 = 16;
-var row17 = $("#17");
-row17 = 17;
-var rowTextArea = ["row9", "row10", "row11", "row12", "row13", "row14", "row15", "row16", "row17"];
-row9 = 9;row10 = 10;row11 = 11;*/
 
 var rowTextArea = ['9','10','11','12','13','14','15','16','17'];
 
-console.log(rowTextArea);
 var currentTime = moment().format("HH");
-var currentTime = 15;
-console.log(currentTime);
 for (var i=0; i<rowTextArea.length; i++) {
-console.log("come here");
 console.log(rowTextArea[i]);
-    if(currentTime == rowTextArea[i])
+    if(currentTime === rowTextArea[i])
     {
        $(".time-block").children().eq(i).children().eq(1).addClass("red");
     }
@@ -51,64 +19,71 @@ console.log(rowTextArea[i]);
        $(".time-block").children().eq(i).children().eq(1).addClass("grey");
   }
 }
-/*var btn9 = $("#9");
-var btn10 = $("#10");
-var btn =["btn9", "btn10"];
 
-function saveText(){
-    if(btn9)
-{
-    localStorage.setItem = $("textarea").val();
-}
-}
+$(".saveBtn").on("click", function(event){
+  var textData = event.target.parentElement.children[1].value;
+  localStorage.setItem(event.target.attributes[0].value, textData);
+});
 
-function loadText(){
-    $("textarea").val(localStorage.textarea9);
-  //  $("textarea").val(localStorage.textarea10);
+$(document).ready(function(){
+  if(localStorage["btn9"] !== null && localStorage["btn9"] !== undefined){
+    var nine = $("<p>"+ localStorage["btn9"] +"</p>");
+    $("#nine").append(nine[0].innerText);
+  } 
+});
 
-}
+  $(document).ready(function(){
+    if(localStorage["btn10"] !== null && localStorage["btn10"] !== undefined){
+      var ten = $("<p>"+ localStorage["btn10"] +"</p>");
+      $("#ten").append(ten[0].innerText);
+    } 
+});
 
-$(window).on('unload', function(){
-  saveText();
-  loadText();
-  });
-  
+$(document).ready(function(){
+  if(localStorage["btn11"] !== null && localStorage["btn11"] !== undefined){
+    var eleven = $("<p>"+ localStorage["btn11"] +"</p>");
+    $("#eleven").append(eleven[0].innerText);
+  }
+});
 
-btn9.click(saveText);
-//btn10.click(saveText)
-*/
-var txtArea11 = $("textarea");
-var txtArea12 = $("textarea");
-var txtArea13 = $("textarea");
-var txtArea14 = $("textarea");
-var txtArea15 = $("textarea");
-var txtArea17 = $("textarea");
-var txtArea18 = $("textarea");
-var textarea = [ "txtArea9","txtArea10","txtArea11","txtArea12","txtArea13", "txtArea14", "txtArea15", "txtArea16", "txtArea17"];
+$(document).ready(function(){
+  if(localStorage["btn12"] !== null && localStorage["btn12"] !== undefined){
+    var twelve = $("<p>"+ localStorage["btn12"] +"</p>");
+    $("#twelve").append(twelve[0].innerText);
+  } 
+});
 
-var textData = { set : function(){
-                    var textData = [];
-                    var txtArea10 = $("textarea");
-                    $('textarea').each(function(){
-                      for(var i=0; i<textarea.length; i++)
-                      {
-                      textData.push(textarea[i].value);
-                      }
-                    }); 
-                    localStorage.textData = JSON.stringify(textData);
-                  },
-                get : function(){
-                  if(localStorage.textData != undefined){
-                    textData = JSON.parse(localStorage.textData);
-                    for(var i=0; i< textData.length;i++){
-                    $("textarea[i]").val(textarea[i].value);
-                  }
-                }
-              }
-            }
-          textData.get();
-          $("button").click(function(){
-            textData.set();
-          });
+$(document).ready(function(){
+  if(localStorage["btn13"] !== null && localStorage["btn13"] !== undefined){
+    var thirteen = $("<p>"+ localStorage["btn13"] +"</p>");
+    $("#thirteen").append(thirteen[0].innerText);
+  } 
+});
 
+$(document).ready(function(){
+  if(localStorage["btn14"] !== null && localStorage["btn14"] !== undefined){
+    var fourteen = $("<p>"+ localStorage["btn14"] +"</p>");
+    $("#fourteen").append(fourteen[0].innerText);
+  } 
+});
 
+$(document).ready(function(){
+  if(localStorage["btn15"] !== null && localStorage["btn15"] !== undefined){
+    var fifteen = $("<p>"+ localStorage["btn15"] +"</p>");
+    $("#fifteen").append(fifteen[0].innerText);
+  } 
+});
+
+$(document).ready(function(){
+  if(localStorage["btn16"] !== null && localStorage["btn16"] !== undefined){
+    var sixteen = $("<p>"+ localStorage["btn16"] +"</p>");
+    $("#sixteen").append(sixteen[0].innerText);
+  }
+});
+
+$(document).ready(function(){
+  if(localStorage["btn17"] !== null && localStorage["btn17"] !== undefined){
+    var seventeen = $("<p>"+ localStorage["btn17"] +"</p>");
+    $("#seventeen").append(seventeen[0].innerText);
+  } 
+});
